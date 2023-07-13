97e3c199fa26a44443136141f55320d66561791d
koldoarretxea <koldo.arretxea@gmail.com>
2023-07-13 16:30:07 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Conexión a la base de datos MySQL
+db = mysql.connector.connect(
+    host='127.0.0.1',
+    port='3306',
+    user='root',
+    password='9mvu5b6W.',
+    database='fullstack_bottega'
+)
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    # Consulta SQL para obtener todos los registros de la tabla
+    query = 'SELECT * FROM items'
+
+    # Ejecutar la consulta
+    cursor = db.cursor()
+    cursor.execute(query)
+
+    # Obtener los resultados y construir la lista de elementos
+    items = []
+    for item in cursor.fetchall():
+        item_data = {
+            'id': item[0],
+            'name': item[1]
+        }
+        items.append(item_data)
+
+    return jsonify(items)
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    item = request.json.get('name')
+    if item:
+        # Consulta SQL para insertar un nuevo registro en la tabla
+        query = 'INSERT INTO items (name) VALUES (%s)'
+
+        # Datos del nuevo elemento
+        item_data = (item,)
+
+        # Ejecutar la consulta
+        cursor = db.cursor()
+        cursor.execute(query, item_data)
+        db.commit()
+
+        return jsonify({'message': 'Item added successfully'})
+    else:
+        return jsonify({'error': 'Invalid item'})
+
+@app.route('/items/<int:index>', methods=['DELETE', 'PUT'])
+def edit_or_delete_item(index):
+    if request.method == 'DELETE':
+        # Consulta SQL para eliminar un registro de la tabla
+        query = 'DELETE FROM items WHERE id = %s'
+
+        # Datos del índice del elemento a eliminar
+        item_index = (index,)
+
+        # Ejecutar la consulta
+        cursor = db.cursor()
+        cursor.execute(query, item_index)
+        db.commit()
+
+        return jsonify({'message': 'Item deleted successfully'})
+    elif request.method == 'PUT':
+        new_name = request.json.get('name')
+        if new_name:
+            # Consulta SQL para actualizar un registro de la tabla
+            query = 'UPDATE items SET name = %s WHERE id = %s'
+
+            # Datos del nuevo nombre y el índice del elemento a editar
+            item_data = (new_name, index)
+
+            # Ejecutar la consulta
+            cursor = db.cursor()
+            cursor.execute(query, item_data)
+            db.commit()
+
+            return jsonify({'message': 'Item edited successfully'})
+        else:
+            return jsonify({'error': 'Invalid item'})
+
+if __name__ == '__main__':
+    app.run()
--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './App.scss';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editItemId, setEditItemId] = useState(null);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/items');
+      setItems(response.data);
+    } catch (error) {
+      console.error('Error fetching items:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddItem = async () => {
+    if (inputValue) {
+      try {
+        await axios.post('http://localhost:5000/items', { name: inputValue });
+        setInputValue('');
+        fetchItems();
+      } catch (error) {
+        console.error('Error adding item:', error);
+      }
+    }
+  };
+
+  const handleEditItem = (id) => {
+    const newName = prompt('Enter the new name');
+    if (newName) {
+      try {
+        axios.put(`http://localhost:5000/items/${id}`, { name: newName });
+        fetchItems();
+      } catch (error) {
+        console.error('Error editing item:', error);
+      }
+    }
+  };
+
+  const handleDeleteItem = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/items/${id}`);
+      fetchItems();
+    } catch (error) {
+      console.error('Error deleting item:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <p className='titulo'>Fullstack operativa CRUD</p>
+      <p className='titulo1'>En esta página se pueden probar las operaciones CRUD que se guardan en una base de datos relacional MySQL.</p>
+
+      <div className='subcontainer'><input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter an item"
+        />
+        <button onClick={handleAddItem}>{editMode ? 'Save' : 'Add'}</button>
+
+        <button onClick={fetchItems}>Read from Database</button>
+      </div>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name}
+            <button className='button-edit' onClick={() => handleEditItem(item.id)}>Edit</button>
+            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
